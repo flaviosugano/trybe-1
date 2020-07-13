3d6a6ca07d5622f42abda271ae07de64f3aaaaea
Alessandra <itsalematos@hotmail.com>
2020-07-13 17:58:08 -0400

    Add Ex.7 - Convertendo notas numéricas para alfabéticas

---
+++ exercises/4.1/converter-notas.js
@@ -0,0 +1,19 @@
+let nota = 102;
+
+if (nota >= 90 && nota <= 100){
+    console.log("Nota A")
+} else if (nota >= 80 && nota <= 89){
+    console.log("Nota B")
+} else if (nota >= 70 && nota <= 79){
+    console.log("Nota C")
+} else if (nota >= 60 && nota <= 69){
+    console.log("Nota D")
+} else if (nota >= 50 && nota <= 59){
+    console.log("Nota E")
+} else if (nota >= 0 && nota < 50){
+    console.log("Nota F")
+} else if (nota > 100){
+    console.log("Nota inválida")
+} else {
+    console.log("Nota inválida")
+}
