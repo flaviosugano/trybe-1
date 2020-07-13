9eb4a3586db189f2637d54f56367d37e5945cf1a
Alessandra <itsalematos@hotmail.com>
2020-07-13 18:17:19 -0400

    Add Ex.8 - Números pares

---
+++ exercises/4.1/par.js
@@ -0,0 +1,9 @@
+let num1 = 2;
+let num2 = 4;
+let num3 = 7;
+
+if (num1 || num2 || num3 %2 == 0){
+    console.log("Tem número par no meio")
+} else {
+    console.log("Não tem número par no meio")
+}
