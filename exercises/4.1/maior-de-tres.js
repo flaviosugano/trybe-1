9c4f4ae08b78bdc7ac6b2308bbec3b39f0d0e20f
Alessandra <itsalematos@hotmail.com>
2020-07-13 16:56:12 -0400

    Add Ex.3 - Maior de três

---
+++ exercises/4.1/maior-de-tres.js
@@ -0,0 +1,13 @@
+let num1 = 60;
+let num2 = 10;
+let num3 = 200;
+
+// if (num1 > num2 && num1 > num3){
+//     console.log(num1);
+// } else if (num2 > num1 && num2 > num3){
+//     console.log(num2)
+// } else {
+//     console.log(num3)
+// }
+
+let maior = num1 > num2 && num1 > num3 ? console.log(num1) : num2 > num1 && num2 > num3 ? console.log(num2) : console.log(num3);
