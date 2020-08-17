6fe57cbf396276c7869b1aa4b1807af112724eb6
Alessandra <itsalematos@hotmail.com>
2020-08-17 15:25:21 -0400

    Adc exercício 1

--- exercises/8_1/conteúdo.js
+++ exercises/8_1/conteúdo.js
@@ -8,12 +8,22 @@ numbers.forEach(multipliesFor2);
 
 console.log([1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10].sort());
 
-funcaoDeComparacao(a, b) {
-  if (a > b) return -1; // aqui poderia ser qualquer numero negativo
-  if (a < b) return 1; // aqui poderia ser qualquer numero positivo
-  if (a == b) return 0;
-} 
+// funcaoDeComparacao(a, b) {
+//   if (a > b) return -1; // aqui poderia ser qualquer numero negativo
+//   if (a < b) return 1; // aqui poderia ser qualquer numero positivo
+//   if (a == b) return 0;
+// } 
 
-funcaoDeComparacao(a, b) {
-  return a - b; // b - a se for decrescente
-}
+// funcaoDeComparacao(a, b) {
+//   return a - b; // b - a se for decrescente
+// }
+
+const notes = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+const verifyNotes = (studentNotes) => {
+  return Object.values(studentNotes).every((note) => note === 'Aprovado');
+};
+console.log(verifyNotes(notes));
