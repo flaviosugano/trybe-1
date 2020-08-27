0a77c8c403a76b2b325b24f8573a057f0769f244
Alessandra <itsalematos@hotmail.com>
2020-08-27 11:31:05 -0400

    Refazendo exercícios callback/assíncrono

--- exercises/9_1/conteudo.js
+++ exercises/9_1/conteudo.js
@@ -1,82 +1,86 @@
-// const assert = require('assert');
+const assert = require('assert');
 
-// const pushNumber = (list, number) => list.push(number);
+const pushNumber = (list, number) => list.push(number);
 
-// let numbers = [];
+let numbers = [];
 
-// pushNumber(numbers, 1);
-// pushNumber(numbers, 2);
-// pushNumber(numbers, 3);
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
 
-// assert.deepEqual(numbers, [1, 2, 3]);
-// // -----------------------------------
-// const assert = require('assert');
+assert.deepEqual(numbers, [1, 2, 3]);
+// -----------------------------------
+const assert = require('assert');
 
-// const pushNumber = (list, number) => list.push(number);
+const pushNumber = (list, number) => list.push(number);
 
-// let numbers = [];
+let numbers = [];
 
-// setTimeout(() => pushNumber(numbers, 1), 3000);
-// pushNumber(numbers, 2);
-// pushNumber(numbers, 3);
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
 
-// assert.deepEqual(numbers, [1, 2, 3]); // essa validação falha
-// //----------------------------
-// const assert = require('assert');
+assert.deepEqual(numbers, [1, 2, 3]); // essa validação falha
+//----------------------------
+const assert = require('assert');
 
-// const pushNumber = (list, number) => {
-//   list.push(number);
-//   console.log(list);
-// };
+const pushNumber = (list, number) => {
+  list.push(number);
+  console.log(list);
+};
 
-// let numbers = [];
+let numbers = [];
 
-// setTimeout(() => pushNumber(numbers, 1), 3000);
-// pushNumber(numbers, 2);
-// pushNumber(numbers, 3);
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
 
-// setTimeout(() => assert.deepEqual(numbers, [2, 3, 1]), 3000);
+setTimeout(() => assert.deepEqual(numbers, [2, 3, 1]), 3000);
 
-// setTimeout(() => alert("1"),5000);
-// alert("2");
+setTimeout(() => alert("1"),5000);
+alert("2");
 
-// const assert = require('assert');
+// EXERCÍCIO 1 - CONTEÚDO 
+const assert = require('assert');
 
-// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-// const getUser = (callback) => {
-//   const userToReturn = {
-//     firstName: "Ivan",
-//     lastName: "Ivanovich",
-//     nationality: "Russian"
-//   };
-//   return callback(userToReturn);
-// };
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  console.log(callback(userToReturn))
+  return callback(userToReturn);
+};
 
-// assert.equal(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
-// assert.equal(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
-// ------------------------------
-// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+assert.equal(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.equal(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
 
-// const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-// const getUser = (callback) => {
-//    setTimeout(() => {
-//     const user = {
-//       firstName: "Ivan",
-//       lastName: "Ivanovich",
-//       nationality: "Russian"
-//     };
-//     return console.log(callback(user));
-//   }, delay());
-// };
+// EXERCÍCIO 2 - CONTEÚDO 
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-// getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
-// getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-// -----------------
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian"
+    };
+    return console.log(callback(user));
+  }, delay());
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+// EXERCÍCIO 3 - CONTEÚDO 
 const countryName = ({ name }) => console.log(`Returned country is ${name}`);
 const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
 
@@ -84,7 +88,7 @@ const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMillisec
 
 const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
 
-const getCountry = (onSuccess, offSucess) => {
+const getCountry = (onSuccess, offSuccess) => {
   setTimeout(() => {
     const didOperationSucceed = Math.random() >= 0.5;
     if(didOperationSucceed) {
@@ -97,7 +101,7 @@ const getCountry = (onSuccess, offSucess) => {
     }
     else {
       const errorMessage = "Country could not be found";
-      offSucess(errorMessage);
+      offSuccess(errorMessage);
     }
   }, delay());
 };
